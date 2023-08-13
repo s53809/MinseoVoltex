0fbe86906c8df5eece8f2a4f90c853970d0a588d
Minsoda <dg4res@gmail.com>
2023-08-14 00:08:39 +0900

    Object Pooling

--- MinseoVoltex/Assets/Scripts/MinseoUtillity/ObjectPool/MonoPooledObject.cs
+++ MinseoVoltex/Assets/Scripts/MinseoUtillity/ObjectPool/MonoPooledObject.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class MonoPooledObject : MonoBehaviour
 {
-    private ObjectPool m_objectPooler;
-
-    protected virtual void Awake()
-    {
-        m_objectPooler = transform.parent.GetComponent<ObjectPool>();
-    }
+    protected ObjectPool m_objectPooler;
+    private Boolean m_isInitialSetting;
 
     protected virtual void OnDisable()
     {
-        
+        if (m_isInitialSetting) m_isInitialSetting = false;
+        else m_objectPooler.RetrieveObject(gameObject);
+    }
+
+    public void InitialSetting()
+    {
+        m_objectPooler = transform.parent.GetComponent<ObjectPool>();
+        m_isInitialSetting = true;
+        gameObject.SetActive(false);
     }
 }
