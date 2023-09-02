d9265f8a5d534045404cb6dc88478fdefab973a1
Minsoda <dg4res@gmail.com>
2023-09-02 16:17:42 +0900

    UIGenerator 제작중 오류났다 하하

--- MinseoVoltex/Assets/Scripts/MinseoUtillity/UI/UIGenerator.cs
+++ MinseoVoltex/Assets/Scripts/MinseoUtillity/UI/UIGenerator.cs
@@ -5,16 +5,19 @@ using System.CodeDom;
 using System.CodeDom.Compiler;
 using Microsoft.CSharp;
 using UnityEngine;
+using System.Security.Permissions;
 
 public class UIGenerator : MonoBehaviour
 {
     private CodeCompileUnit targetUnit;
     private CodeTypeDeclaration targetClass;
 
-    [InspectorButton("Generate View Code")]
+    [InspectorButton("Generate View Code", true)]
     private void StartSequence()
     {
-        GenerateCode($"{gameObject.name}View");
+        GenerateCode(
+            Path.Combine(
+            Application.dataPath, @"Scripts/UI", gameObject.name, $"{gameObject.name}View.cs"));
     }
 
     private void GenerateCode(String pFileName)
@@ -29,18 +32,36 @@ public class UIGenerator : MonoBehaviour
         targetUnit.Namespaces.Add(samples);
 
         //Generate Member
+        CodeMemberField[] fields = GenerateMember();
+        foreach (CodeMemberField field in fields)
+        {
+            targetClass.Members.Add(field);
+        }
 
-        
+        //Generate Code
+        CodeDomProvider provider = CodeDomProvider.CreateProvider("CSharp");
+        CodeGeneratorOptions options = new CodeGeneratorOptions();
+        options.BracingStyle = "C";
+        using(StreamWriter sourceWriter = new StreamWriter(pFileName))
+        {
+            
+            provider.GenerateCodeFromCompileUnit(targetUnit, sourceWriter, options);
+        }
     }
 
-    private CodeMemberProperty[] GenerateMember()
+    private CodeMemberField[] GenerateMember()
     {
         Int32 childCount = gameObject.transform.childCount;
-        CodeMemberProperty[] temp = new CodeMemberProperty[childCount];
+        CodeMemberField[] temp = new CodeMemberField[childCount];
 
         for(Int32 i = 0; i < childCount; i++)
         {
-            
+            GameObject childObj = gameObject.transform.GetChild(i).gameObject;
+            CodeMemberField field = new CodeMemberField();
+            field.Attributes = MemberAttributes.Public;
+            field.Name = childObj.name;
+            field.Type = FindUIComponent(childObj);
+            temp[i] = field;
         }
 
         return temp;
@@ -48,7 +69,15 @@ public class UIGenerator : MonoBehaviour
 
     private CodeTypeReference FindUIComponent(GameObject obj)
     {
-        //obj.TryGetComponent()
+        UITypes typeFinder = new UITypes();
+        Component comp = new Component();
+        foreach(Type type in typeFinder)
+        {
+            if(obj.TryGetComponent(type, out comp))
+            {
+                return new CodeTypeReference(comp.GetType());
+            }
+        }
         throw new NotImplementedException();
     }
 }
--- MinseoVoltex/Assets/Scripts/MinseoUtillity/UI/UITypes.cs
+++ MinseoVoltex/Assets/Scripts/MinseoUtillity/UI/UITypes.cs
@@ -1,9 +1,32 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public static class UITypes
+public class UITypes : IEnumerator, IEnumerable
 {
-    
+    private Type[] types = new Type[]
+    {
+        typeof(UnityEngine.UI.Text),
+        typeof(UnityEngine.UI.Button),
+        typeof(UnityEngine.UI.Image),
+        typeof(UnityEngine.UI.Dropdown),
+        typeof(UnityEngine.UI.InputField),
+        typeof(UnityEngine.UI.ScrollRect),
+        typeof(UnityEngine.GameObject),
+    };
+    private Int32 index = 0;
+    public object Current => types[index];
+
+    public IEnumerator GetEnumerator() => types[index] as IEnumerator;
+
+    public bool MoveNext()
+    {
+        if (index < types.Length)
+        {
+            index++;
+            return true;
+        }
+        else return false;
+    }
+
+    public void Reset() => index = 0;
 }
